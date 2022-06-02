6d2cb26f61ddf44e0c6b0137180fa158f2c7b5c0
gandhi <gandhi@gg.com>
2022-06-02 10:19:05 +0000

    War file format changed

--- src/main/java/com/devops/assignment/DevOpsProjectApplication.java
+++ src/main/java/com/devops/assignment/DevOpsProjectApplication.java
@@ -2,9 +2,10 @@ package com.devops.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DevOpsProjectApplication {
+public class DevOpsProjectApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevOpsProjectApplication.class, args);
